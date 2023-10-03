f2c2cb2c6ecf31e2614cd21ebcf69012be43aaee
Aditya-Vrma <vermaaditya77777@gmail.com>
2023-10-03 21:13:29 +0530

    Putting Content Inside Login Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,21 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./Header";
 import Post from "./Post.jsx";
+import { Route, Routes } from "react-router";
+import Layout from "./Layout";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <main>
-      <Header />
-      <Post />
-      <Post />
-      <Post />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
+    </Routes>
   );
 }
 
