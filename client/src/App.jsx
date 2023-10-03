c8f11e333025c41366f22bfac634d7b6e0a14b1d
Aditya-Vrma <vermaaditya77777@gmail.com>
2023-10-03 22:27:23 +0530

    Now adding backend (0.43.00)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import { Route, Routes } from "react-router";
 import Layout from "./Layout";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,6 +16,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   );
